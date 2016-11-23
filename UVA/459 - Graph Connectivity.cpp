43bfa1d31579459cff5aec68a7407a6368075783
AliOsm <AliOsm1997@gmail.com>
2016-11-23 16:58:16 +0200

    Solve problem 459 - Graph Connectivity from UVA

---
+++ UVA/459 - Graph Connectivity.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int> > g;
+int vis[27];
+
+void DFS(int n) {
+	vis[n] = 1;
+	for (int i = 0; i < g[n].size(); i++)
+		if(!vis[g[n][i]])
+			DFS(g[n][i]);
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		g.clear();
+		for (int i = 0; i < 27; i++) vis[i] = 0;
+
+		char tmp;
+		cin >> tmp;
+		int end = tmp - 'A' + 1;
+		g.resize(end);
+
+		for (int i = 0; i < end; i++) g[i].push_back(i);
+
+		string s;
+		cin.ignore();
+		while (getline(cin, s) && s != "") {
+			int a = s[0] - 'A';
+			int b = s[1] - 'A';
+			g[a].push_back(b);
+			g[b].push_back(a);
+		}
+
+		int c = 0;
+		for (int i = 0; i < g.size(); i++)
+			for(int j = 0; j < g[i].size(); j++)
+				if (!vis[g[i][j]]) {
+					c++;
+					DFS(g[i][j]);
+				}
+
+		cout << c << endl;
+		if (t) cout << endl;
+	}
+
+	return 0;
+}
--- UVA/README.md
+++ UVA/README.md
@@ -1,5 +1,6 @@
 # Problems
 
 - [439 - Knight Moves](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=380)
+- [459 - Graph Connectivity](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=400)
 - [11362 - Phone List](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2347)
 - [11917 - Do Your Own Homework](https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3068)
