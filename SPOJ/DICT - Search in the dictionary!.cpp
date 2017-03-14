3f9ba66cdffa24e4431eefe7ff5b730cb723569d
aliosm <aliosm1997@gmail.com>
2017-03-14 21:11:33 +0200

    Updated problem DICT - Search in the dictionary! code from SPOJ

--- SPOJ/DICT - Search in the dictionary!.cpp
+++ SPOJ/DICT - Search in the dictionary!.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 #include <string>
-#include <set>
+#include <vector>
+#include <algorithm>
 #include <memory.h>
 
 using namespace std;
 
 struct trie {
   bool isEnd;
-  int prefixes;
   trie *next[26];
-  
+
   trie() {
     isEnd = false;
-    prefixes = 0;
     for(int i = 0; i < 26; i++)
       next[i] = NULL;
   }
 };
 
-set<string> words;
-set<string>::iterator it;
+vector<string> words;
 void DFS(trie *cursor, string word) {
+  if(cursor->isEnd) {
+    if(word != "")
+      words.push_back(word);
+  }
+
   for(int i = 0; i < 26; i++)
     if(cursor->next[i] != NULL)
       DFS(cursor->next[i], word + char(i + 'a'));
-  
-  if(cursor->isEnd) {
-    if(word != "")
-      words.insert(word);
-    return;
-  }
 }
 
 trie *root;
 
 void insert(string s) {
   trie *cursor = root;
-  
+
   int number;
   for(int i = 0; i < s.length(); i++) {
     number = s[i] - 'a';
-    
+
     if(cursor->next[number] == NULL)
       cursor->next[number] = new trie;
-    
+
     cursor = cursor->next[number];
-    cursor->prefixes++;
   }
 
   cursor->isEnd = true;
@@ -63,27 +59,27 @@ void freeTrie(trie* cursor) {
 
 int main() {
   root = new trie;
-  
+
   int n;
   cin >> n;
-  
+
   string s;
   while(n--) {
     cin >> s;
     insert(s);
   }
-  
+
   int m;
   cin >> m;
-  
+
   bool pass;
   for(int c = 1; m--; c++) {
     pass = true;
     cout << "Case #" << c << ':' << endl;
-    
+
     cin >> s;
     trie *cursor = root;
-  
+
     for(int i = 0; i < s.length(); i++)
       if(cursor->next[s[i] - 'a'] != NULL)
         cursor = cursor->next[s[i] - 'a'];
@@ -91,21 +87,20 @@ int main() {
         pass = false;
         break;
       }
-    
+
     if(cursor == root || !pass) {
       cout << "No match." << endl;
       continue;
     }
-    
+
     words.clear();
     DFS(cursor, "");
-    
-    for(it = words.begin(); it != words.end(); it++)
-      cout << s << *it << endl;
+
+    for(int i = 0; i < words.size(); i++)
+      cout << s << words[i] << endl;
   }
-  
+
   freeTrie(root);
-  
+
   return 0;
 }
-
