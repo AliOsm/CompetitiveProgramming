26eecd9e138a05c10ae2ae4c1919e53b1673b604
RahulPatil07109 <63510610+RahulPatil07109@users.noreply.github.com>
2021-02-27 12:35:33 +0530

    Update 192A. Funky Numbers.cpp
    some changes also works (time 62ms). But great logic man!!

--- CodeForces/192A. Funky Numbers.cpp
+++ CodeForces/192A. Funky Numbers.cpp
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-int const N = 1e5 + 1;
 int n;
 set<long long> st;
 
 int main() {
   scanf("%d", &n);
 
-  for(int i = 1; i < N; ++i)
-    st.insert(1ll * i * (i + 1) / 2);
+  for(int i = 1; i*(i+1)/2 < n; ++i)
+    st.insert(i * (i + 1) / 2);
   
-  for(set<long long>::iterator it = st.begin(); it != st.end(); ++it) {
-    if(n - (*it) > 0 && st.count(n - (*it))) {
+  for(auto it = st.begin(); it != st.end(); ++it) {
+    if(st.count(n - (*it))) {
       puts("YES");
       return 0;
     }
