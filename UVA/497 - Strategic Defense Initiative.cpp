2e2035c707c7c1f64899f1be75b11242ea012dd1
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2022-09-16 20:45:12 +0300

    Update 497 - Strategic Defense Initiative.cpp

--- UVA/497 - Strategic Defense Initiative.cpp
+++ UVA/497 - Strategic Defense Initiative.cpp
@@ -15,51 +15,47 @@ int main() {
 
 	while(cases-- != 0) {
 		memset(p, -1, sizeof p);
-		memset(t, 0, sizeof t);
 		memset(d, 0, sizeof d);
 
 		for(int i = 0; getline(cin, s) && s != ""; ++i) {
 			a[i] = stoi(s);
 			n = i;
 		}
-
 		++n;
 
 		int l = 1;
-	    t[0] = a[0];
+		t[0] = a[0];
 
-	    for(int i = 1, idx; i < n; ++i) {
-	      idx = upper_bound(t, t + l, a[i]) - t;
+		for(int i = 1; i < n; ++i) {
+			int idx = upper_bound(t, t + l, a[i]) - t;
 
-	      if (idx > 0 && a[i] > t[idx - 1]) {
-		      t[idx] = a[i];
-		      d[idx] = i;
-		      p[d[idx]] = d[idx - 1];
-		  } else if(idx == 0) {
-		      t[idx] = a[i];
-		      d[idx] = i;
-		      p[d[idx]] = d[idx - 1];
-		  }
+			if((idx > 0 && a[i] > t[idx - 1]) || idx == 0) {
+				t[idx] = a[i];
+				d[idx] = i;
+				p[i] = d[idx - 1];
 
-	      if(idx == l) {
-	          ++l;
-	      }
-	    }
+				if(idx == l) {
+					++l;
+				}
+			}
+		}
 
-	    printf("Max hits: %d\n", l);
+		printf("Max hits: %d\n", l);
 
-	    int cur = d[l - 1];
-	    int idx = l - 1;
-	    do {
-	    	sol[idx--] = a[cur];
-	    	cur = p[cur];
-	    } while(cur != -1);
+		int cur = d[l - 1];
+		int idx = l - 1;
+		do {
+			sol[idx--] = a[cur];
+			cur = p[cur];
+		} while(cur != -1);
 
-	    for (int i = 0; i < l; ++i) {
-	    	printf("%d\n", sol[i]);
-	    }
+		for(int i = 0; i < l; ++i) {
+			printf("%d\n", sol[i]);
+		}
 
-	    puts("");
+		if(cases) {
+			puts("");
+		}
 	}
 
 	return 0;
