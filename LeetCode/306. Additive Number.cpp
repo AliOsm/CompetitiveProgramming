de593436a2129591a574c2d578ca7ce8a962d1f2
Ali Hamdi Ali Fadel <AliOsm1997@gmail.com>
2023-01-06 14:58:26 +0300

    Create 306. Additive Number.cpp

---
+++ LeetCode/306. Additive Number.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    bool isAdditiveNumberRec(string num, int idx, long long prev1, long long prev2, int cnt) {
+        cout << num << ' ' << idx << ' ' << prev1 << ' ' << prev2 << endl;
+
+        if(idx > num.length()) {
+            return false;
+        }
+
+        if(idx == num.length() && prev1 != -1 && prev2 != -1 && cnt > 2) {
+            return true;
+        } else if (idx == num.length() && (prev1 == -1 || prev2 == -1 || cnt <= 2)) {
+            return false;
+        }
+
+        bool ret = false;
+
+        if(prev1 == -1 && prev2 == -1) {
+            for(int i = idx + 1; i < idx + 18; ++i) {
+                if(num[idx] != '0' || (num[idx] == '0' && i == 1)) {
+                    ret |= isAdditiveNumberRec(num, i, stoll(num.substr(0, i)), -1, cnt + 1);
+                }
+            }
+        } else if(prev2 == -1) {
+            for(int i = idx + 1; i < idx + 18; ++i) {
+                if(num[idx] != '0' || (num[idx] == '0' && i - idx == 1)) {
+                    ret |= isAdditiveNumberRec(num, i, stoll(num.substr(idx, i - idx)), prev1, cnt + 1);
+                }
+            }
+        } else {
+            for(int i = idx + 1; i < idx + 18; ++i) {
+                long long nxt = stoll(num.substr(idx, i - idx));
+                if((num[idx] != '0' || (num[idx] == '0' && i - idx == 1)) && prev1 + prev2 == nxt) {
+                    ret |= isAdditiveNumberRec(num, i, nxt, prev1, cnt + 1);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+public:
+    bool isAdditiveNumber(string num) {
+        return isAdditiveNumberRec(num, 0, -1, -1, 0);
+    }
+};
